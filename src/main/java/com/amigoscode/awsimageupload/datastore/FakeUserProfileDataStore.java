aa64298ea08a84248d6195c0c1a0f5f2cecc11b3
James <viess009>
2021-01-19 21:54:15 -0600

    Added in-memory Database

---
+++ src/main/java/com/amigoscode/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -0,0 +1,24 @@
+package com.amigoscode.awsimageupload.datastore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.amigoscode.awsimageupload.profile.UserProfile;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+	private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+	
+	static {
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "janetjones", null));
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "antoniojunior", null));
+	}
+	
+	public List<UserProfile> getUserProfiles() {
+		return USER_PROFILES;
+	}
+}
