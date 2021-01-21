94bc07b149c24ab245e71ead9ac7eb4726adf7a0
James <viess009>
2021-01-20 22:24:15 -0600

    Storing Amazon Credentials outside of project.

--- src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -1,5 +1,9 @@
 package com.amigoscode.awsimageupload.config;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +18,31 @@ public class AmazonConfig {
 
 	@Bean
 	public AmazonS3 s3() {
-		AWSCredentials awsCredentials = new BasicAWSCredentials(
-			"AKIAJHIQPHOWSHTTCGSQ",
-			"mWVPqV8eYl0vogtwLLKyvHfEvIfjShTyj5BhLwaK"
-		);
+		AWSCredentials awsCredentials = null;
+		try {
+			awsCredentials = new BasicAWSCredentials(getKey("AWSAccessKeyId"), getKey("AWSSecretKey"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
-	return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+		return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
 	}
+	
+	private String getKey(String keyType) throws Exception {
+		File file = new File("C:\\Users\\James\\.aws\\credentials\\rootkey.csv");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		String line = br.readLine();
+		while (line != null) {
+			if (line.startsWith(keyType)) {
+				br.close();
+				return line.split("=")[1]; // returns value of key value pair
+			}
+			line = br.readLine();
+		}
+		br.close();
+		return null;
+	}
+	
 }
